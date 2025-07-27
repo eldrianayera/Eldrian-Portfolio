bd6e131c4933fd0282612839524f2bd8fbae47e4
Eldrian <eldrianayera710@gmail.com>
2025-07-27 22:14:57 +0800

    meteor-web : add Home and NotFound page

--- Meteor-web/src/App.jsx
+++ Meteor-web/src/App.jsx
@@ -1,13 +1,14 @@
-import Header from "./Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Header />} />
-          <Route path="*" element={<Header />} />
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- Meteor-web/src/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1>Welcome to Meteor</h1>;
-}
-
-export default Header;
