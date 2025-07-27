96fcc3f0f95c51aa95ba8993cf00ea33e9e0d0ca
Eldrian <eldrianayera710@gmail.com>
2025-07-27 23:39:46 +0800

    meteor-web : complete dark mode toggle button

--- Meteor-web/src/components/ThemeToggle.jsx
+++ Meteor-web/src/components/ThemeToggle.jsx
@@ -1,6 +1,7 @@
 import { Sun, Moon } from "lucide-react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -29,7 +30,13 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <button onClick={toggleTheme}>
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+      )}
+    >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
       ) : (
