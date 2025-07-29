994ef5e0c96285afb4e669b3e0e92bdd98c69c66
Eldrian <eldrianayera710@gmail.com>
2025-07-29 17:48:09 +0800

    meteor-web : complete Contact UI

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,25 +4,27 @@ import {
   Mail,
   MessageCircleMore,
   Phone,
+  Send,
 } from "lucide-react";
+import { cn } from "../lib/utils";
 
 export const ContactSection = () => {
   return (
-    <section className="min-h-screen py-24 px-8" id="contact">
-      <div className="container mx-auto flex flex-col items-center">
+    <section className="min-h-screen py-24 px-8 relative" id="contact">
+      <div className="container mx-auto max-w-5xl">
         {" "}
         <h2 className="text-3xl md:text-4xl font-bold mb-7 text-center">
           Get in <span className="text-primary"> Touch</span>
         </h2>
-        <div className="max-w-2xl">
+        <div>
           {" "}
-          <p>
+          <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
             Have a project in mind or just want to say hello? I'm always open to
             new opportunities, collaborations, or a friendly chat. Let’s
             connect!
           </p>
-          <div className="grid grid-cols-1 md:grid-cols-2 mt-12">
-            <div className="flex flex-col items-center gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 mt-12 gap-8 ">
+            <div className="flex flex-col items-center gap-4 border-2 p-9 rounded-2xl justify-evenly">
               <h3 className="text-xl font-bold">Contact Information</h3>
 
               <div className="flex flex-col gap-3">
@@ -66,7 +68,50 @@ export const ContactSection = () => {
                 </div>
               </div>
             </div>
-            <div></div>
+            <div className="flex flex-col items-center gap-4 border-2 p-9 rounded-2xl">
+              <div className="flex flex-col items-center w-full ">
+                <h3 className="text-xl font-bold mb-5">Send a Message</h3>
+                <form action="" className="w-full mb-5">
+                  <div className="flex flex-col items-center gap-2 w-full mb-5">
+                    <p>Your Name</p>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="John Doe..."
+                      className=" bg-card rounded-md border-2 py-1 px-2 w-full focus:outline-hidden focus:ring-2 focus:ring-primary"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-2 w-full mb-5">
+                    <p>Your Email</p>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="johndoe@gmail.com"
+                      className=" bg-card rounded-md border-2 py-1 px-2 w-full focus:outline-hidden focus:ring-2 focus:ring-primary"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-2 w-full mb-5">
+                    <p>Your Message</p>
+                    <textarea
+                      id="message"
+                      name="message"
+                      type="text"
+                      placeholder="Hello , I'd like to talk about..."
+                      className="resize-none bg-card rounded-md border-2 py-1 px-2 w-full h-20 focus:outline-hidden focus:ring-2 focus:ring-primary"
+                    />
+                  </div>
+                </form>
+                <button
+                  className={cn(
+                    "cosmic-button w-full flex justify-center gap-2"
+                  )}
+                >
+                  Send Message <Send />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
